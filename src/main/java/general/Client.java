8fd6752aa59d880602679f3edc2f28e8f195f037
mehdisellami <mehdisellami9@gmail.com>
2020-10-20 16:52:52 +0200

    Td3

--- src/main/java/general/Client.java
+++ src/main/java/general/Client.java
@@ -1,24 +1,31 @@
 package general;
 
+import java.util.Collection;
+
 public class Client implements PrePostVisitable,Visitable {
 
 
     String name;
 
+    Collection<Commande> commandes;
+    Collection <Ligne> lignes;
 
 
     public Client(String name) {
         this.name = name;
     }
 
-    void addCommande(Commande commande){
 
-        new Commande("");
 
+
+    void addLigne(String name , Ligne ligne){
+        new Client (name);
+        lignes.add(ligne);
     }
 
+    public void addCommande( Commande commande){
+        commandes.add(commande);
 
-    void addLigne(String ch , Ligne ligne){
     }
 
 
